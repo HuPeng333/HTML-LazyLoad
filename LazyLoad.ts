68fd3a305cb253347912b855bf61062bf1087993
HuPeng <2237803016@qq.com>
2021-06-06 23:59:46 +0800

    v1.0.0 详细请见README.md

---
+++ LazyLoad.ts
@@ -0,0 +1,80 @@
+type LazyLoadConstructor = {
+    scrollElementSelector: Document | string,
+    lazyLoadElementsSelector: Array<string>,
+    onenter?: Function,
+    onleave?: Function
+}
+class LazyLoad{
+    private readonly scrollElement: HTMLElement | Document// 滚动容器元素
+    private readonly lazyLoadElements: Array<Element> // 懒加载元素
+    private readonly onenter: Function | undefined
+    private readonly onleave: Function | undefined
+
+    constructor(LazyLoad: LazyLoadConstructor) {
+        // 搜索父元素
+        if (typeof LazyLoad.scrollElementSelector === 'string') {
+            const scrollElement = document.getElementById(LazyLoad.scrollElementSelector)
+            if (!scrollElement) {
+                throw new Error('没有找到滚动容器选择器对应的元素')
+            }
+            this.scrollElement = scrollElement
+        } else {
+            this.scrollElement = document
+        }
+
+        // 搜索懒加载元素
+        const lazyLoadElements: Array<Element> = []
+        LazyLoad.lazyLoadElementsSelector.forEach((value, index) => {
+            // 遍历选择器数组
+            const nodeList = document.querySelectorAll(value)
+            if (nodeList.length === 0) {
+                console.warn('懒加载选择器中有无法找到的元素,索引: ' + index)
+            } else {
+                nodeList.forEach(value1 => {
+                    lazyLoadElements.push(value1)
+                })
+            }
+        })
+        if (lazyLoadElements.length === 0) {
+            throw new Error('没有找到可用的懒加载元素')
+        }
+        this.lazyLoadElements = lazyLoadElements
+        // 绑定回调函数
+        this.onenter = LazyLoad.onenter
+        this.onleave = LazyLoad.onleave
+    }
+    /**
+     * 开始监听
+     */
+    public start ():void {
+        console.log(this.scrollElement)
+        // 给父元素绑定滚动事件
+        this.scrollElement.addEventListener('scroll', () => {
+            this.lazyLoadElements.forEach(element => {
+                // @ts-ignore 懒加载元素到父元素顶部的距离
+                const elementOffsetTop  = element.offsetTop
+                if (this.scrollElement === document ) {
+                    // 非区域滚动
+                    if (elementOffsetTop - this.scrollElement.documentElement.scrollTop - window.innerHeight <= 0) {
+                        this.onenter ? this.onenter() : ''
+                    }
+                } else {
+                    // 区域滚动
+                    // @ts-ignore 父元素滚动条已滚动高度
+                    const scrollElementScrollTop = this.scrollElement.scrollTop
+                    // @ts-ignore 父元素可用高度
+                    const scrollElementHeight = this.scrollElement.offsetHeight
+                    console.log(elementOffsetTop, scrollElementScrollTop, scrollElementHeight)
+                    if (elementOffsetTop - scrollElementScrollTop - scrollElementHeight <= 0) {
+                    }
+                }
+            })
+        })
+    }
+
+    /**
+     * 停止监听
+     */
+    public stop ():void{
+    }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+#元素懒加载
+
+##使用方法
+1.引入js文件
+
+2.创建一个懒加载对象
+```
+    const lazyLoad = new LazyLoad({
+        scrollElementSelector: 'id选择器,不需要加#号或者直接填写document',
+        lazyLoadElementsSelector: ['类选择器,不需要加点']
+    })
+    
+    // 例如
+    const lazyLoad = new LazyLoad({
+        scrollElementSelector: document,
+        lazyLoadElementsSelector: ['item1', 'item2']
+    })
+    
+    const lazyLoad2 = new LazyLoad({
+        scrollElementSelector: 'parent',
+        lazyLoadElementsSelector: ['item1']
+    })
+    
+```
+3.调用开始方法
+```
+    lazyLoad.start()
+```
+
+4.停止监听
+```
+    lazyLoad.stop()
+```
+
+##注意事项
+
+1.如果使用区域滚动,则父元素需要设置定位 如 ***position: relative***
+2.stop()方法暂时没有 (*因为ts太麻烦了啊啊啊!!!*)
